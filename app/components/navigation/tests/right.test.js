0c733ca3fe61b65a86d8ae4fa3694151c3e2c094
Marat Fakhreev <marat.fakhreev@flatstack.com>
2016-05-22 14:18:56 +0300

    Write Sign In Modal tests

--- app/components/navigation/tests/right.test.js
+++ app/components/navigation/tests/right.test.js
@@ -8,14 +8,14 @@ import SessionActions from 'actions/session';
 import SigninActions from 'actions/signin';
 import SignupActions from 'actions/signup';
 
-describe('NavigationLeft', () => {
+describe('NavigationRight', () => {
   describe('when user is signed in', () => {
     beforeEach(() => {
       spyOn(session, 'loggedIn').and.returnValue(true);
     });
 
     it('renders NavigationRight with Nav and NavItem', () => {
-      const navigationRightComponent = mount(<NavigationRight />);
+      const navigationRightComponent = mount(<NavigationRight/>);
       const navs = navigationRightComponent.find(Nav);
       const navItems = navigationRightComponent.find(NavItem);
 
@@ -27,7 +27,7 @@ describe('NavigationLeft', () => {
 
     it('calls TodoActions.show()', () => {
       spyOn(TodoActions, 'show');
-      const navigationRightComponent = mount(<NavigationRight />);
+      const navigationRightComponent = mount(<NavigationRight/>);
       navigationRightComponent.find('a').at(0).simulate('click');
 
       expect(TodoActions.show).toHaveBeenCalled();
@@ -35,7 +35,7 @@ describe('NavigationLeft', () => {
 
     it('calls SessionActions.delete', () => {
       spyOn(SessionActions, 'delete');
-      const navigationRightComponent = mount(<NavigationRight />);
+      const navigationRightComponent = mount(<NavigationRight/>);
       navigationRightComponent.find('a').at(1).simulate('click');
 
       expect(SessionActions.delete).toHaveBeenCalled();
@@ -44,7 +44,7 @@ describe('NavigationLeft', () => {
 
   describe('when user is not signed in', () => {
     it('renders NavigationRight with Nav and NavItem', () => {
-      const navigationRightComponent = mount(<NavigationRight />);
+      const navigationRightComponent = mount(<NavigationRight/>);
       const navs = navigationRightComponent.find(Nav);
       const navItems = navigationRightComponent.find(NavItem);
 
@@ -56,7 +56,7 @@ describe('NavigationLeft', () => {
 
     it('calls SignupActions.show()', () => {
       spyOn(SignupActions, 'show');
-      const navigationRightComponent = mount(<NavigationRight />);
+      const navigationRightComponent = mount(<NavigationRight/>);
       navigationRightComponent.find('a').at(0).simulate('click');
 
       expect(SignupActions.show).toHaveBeenCalled();
@@ -64,7 +64,7 @@ describe('NavigationLeft', () => {
 
     it('calls SessionActions.delete', () => {
       spyOn(SigninActions, 'show');
-      const navigationRightComponent = mount(<NavigationRight />);
+      const navigationRightComponent = mount(<NavigationRight/>);
       navigationRightComponent.find('a').at(1).simulate('click');
 
       expect(SigninActions.show).toHaveBeenCalled();
